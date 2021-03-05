0c72485d0866dea4d8a2ef26441f63a70b0aa97f
billytrinh <quanghoa.trinh@gmail.com>
2021-03-05 09:36:07 +0700

    demo s7

--- src/session/s6/Male.java
+++ src/session/s6/Male.java
@@ -1,10 +1,32 @@
 package session.s6;
 
-public class Male extends Human {
+import session.s7.IHuman;
+import session.s7.IMale;
 
+public class Male extends Human implements IHuman, IMale {
 
     public void showInfo() {
         System.out.println("Name: "+name);
         System.out.println("Birthday:"+birthday);
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void jump() {
+
+    }
+
+    @Override
+    public void kiss() {
+
+    }
+
+    @Override
+    public void playGame() {
+
     }
 }
